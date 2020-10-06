ee106c82a91e5d8055cbf714130f1ba04b91fb5b
AmirhoseinArabhaji <a.arab12345@gmail.com>
2020-10-06 19:47:22 +0330

    doctor models created

--- Backend/Teledoctor/doctor/models.py
+++ Backend/Teledoctor/doctor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Doctor(models.Model):
+    
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    birthday = models.DateField(auto_now=False, auto_now_add=False)
+    phone_number = models.PositiveIntegerField(min_length=11, max_length=11, blank=False)
+    email = models.EmailField(blank=True)
+    social_id = models.PositiveIntegerField(min_length=10, max_length=10, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name + self.last_name
+    
