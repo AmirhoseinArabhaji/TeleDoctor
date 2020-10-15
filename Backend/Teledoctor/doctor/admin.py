362f45f9a03920016236244cbae3bbd482a21f4b
AmirhoseinArabhaji <a.arab12345@gmail.com>
2020-10-15 12:36:44 +0330

    add customized admin page for Doctor and Patient

--- Backend/Teledoctor/doctor/admin.py
+++ Backend/Teledoctor/doctor/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 
 from .models import Doctor
 
-admin.site.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+
+    list_display = ('first_name', 'last_name', 'email', 'social_id', 'mc_code')
+    search_fields =('email', 'last_name', 'mc_code', 'social_id')
+
+    def first_name(self, obj):
+        return obj.user.first_name
+
+    def last_name(self, obj):
+        return obj.user.last_name
+
+    def email(self, obj):
+        return obj.user.email
+
+    def social_id(self, obj):
+        return obj.user.social_id
+
+
+admin.site.register(Doctor, DoctorAdmin)
--- Backend/Teledoctor/patient/admin.py
+++ Backend/Teledoctor/patient/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 
 from .models import Patient, Visit
 
-admin.site.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+
+    list_display = ('first_name', 'last_name', 'email', 'social_id',)
+    search_fields =('email', 'last_name', 'social_id')
+
+    def first_name(self, obj):
+        return obj.user.first_name
+
+    def last_name(self, obj):
+        return obj.user.last_name
+
+    def email(self, obj):
+        return obj.user.email
+
+    def social_id(self, obj):
+        return obj.user.social_id
+
+
+admin.site.register(Patient, PatientAdmin)
 admin.site.register(Visit)
