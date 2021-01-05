3430b81d538f7d25263fcf30511ff7ef7029ec11
Amirhosein <a.arab12345@gmail.com>
2021-01-05 20:49:05 +0330

    add postgreSQL database settings

--- Backend/Teledoctor/Teledoctor/settings.py
+++ Backend/Teledoctor/Teledoctor/settings.py
@@ -93,10 +93,21 @@ WSGI_APPLICATION = 'Teledoctor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'TeleDoctor',
+        'USER': 'postgres',
+        'PASSWORD': 'amirthemaster',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
